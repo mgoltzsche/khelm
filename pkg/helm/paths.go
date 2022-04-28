d6bcf2f9d1ba64251c7a9604f5f9f517f351c475
ndbrew <ndbsignups+gitea@gmail.com>
2022-04-28 00:38:12 -0700

    fix: Support custom protocols in valueFiles

--- pkg/helm/paths.go
+++ pkg/helm/paths.go
@@ -1,12 +1,19 @@
 package helm
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 )
 
 func absPaths(paths []string, baseDir string) []string {
 	abs := make([]string, len(paths))
 	for i, path := range paths {
+		s_path := strings.Split(path, "://")
+		if len(s_path) == 2 {
+			abs[i] = fmt.Sprintf("%s://%s", s_path[0], absPath(s_path[1], baseDir))
+			continue
+		}
 		abs[i] = absPath(path, baseDir)
 	}
 	return abs
